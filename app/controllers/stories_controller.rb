850dd542150b55db07f722d7d1707374a60d281a
Nicolas Agathos <nicolas.agathos@gmail.com>
2021-11-29 21:24:47 +0100

    stories controller

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,53 @@
+class StoriesController < ApplicationController
+  before_action :set_business, only: [:new, :create]
+  before_action :set_story, only: [:edit, :update, :destroy, :show]
+
+
+  def show
+  end
+
+  def index
+    @stories = Story.all
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.business = @business
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      @story = Story.new
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to story_path(@story)
+    else
+      set_story
+      render :edit
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :description, :published)
+  end
+
+  def set_story
+    @story = Story.find(params[:id])
+  end
+
+  def set_business
+    @business = Business.find(params[:business_id])
+  end
+end
